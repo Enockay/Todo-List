6c33b21cb4be72accbe6f0307e9da19c3cd03163
Enockay <enockaymwema@gmail.com>
2023-08-24 14:25:39 +0300

    Updates

--- main.bundle.js
+++ main.bundle.js
@@ -1,13 +1,4 @@
-/*
- * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
- * This devtool is neither made for production nor for readable output files.
- * It uses "eval()" calls to create a separate source file in the browser devtools.
- * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
- * or disable the default devtool with "devtool: false".
- * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
- */
-/******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
+(self["webpackChunktodo_list"] = self["webpackChunktodo_list"] || []).push([["main"],{
 
 /***/ "./src/main.js":
 /*!*********************!*\
@@ -15,18 +6,20 @@
   \*********************/
 /***/ (() => {
 
-eval("\n\n//# sourceURL=webpack://todo-list/./src/main.js?");
+class projectDone{
+    constructor(project1,project2){
+        this.project1 = project1;
+        this.project2 = project2;
+    }
+}
+
 
 /***/ })
 
-/******/ 	});
-/************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/main.js"]();
-/******/ 	
-/******/ })()
-;
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/main.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7QUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90b2RvLWxpc3QvLi9zcmMvbWFpbi5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjbGFzcyBwcm9qZWN0RG9uZXtcbiAgICBjb25zdHJ1Y3Rvcihwcm9qZWN0MSxwcm9qZWN0Mil7XG4gICAgICAgIHRoaXMucHJvamVjdDEgPSBwcm9qZWN0MTtcbiAgICAgICAgdGhpcy5wcm9qZWN0MiA9IHByb2plY3QyO1xuICAgIH1cbn1cbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
